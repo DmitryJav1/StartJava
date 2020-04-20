5873447d671fecfa877ad81bdc80f204ac743158
DmitryJav1 <krackin97@gmail.com>
2020-04-20 11:49:01 +0300

    Jaeger.java JaegerTest.java 5 errors

--- Lesson_2/Jaeger.java
+++ Lesson_2/Jaeger.java
@@ -1,7 +1,27 @@
 public class Jaeger {
-	public String name;
-	public String launchDate;
-	public int mark;
-	public String status;
-	public String origin;
+	private String name;
+	private String launchDate;
+	private int mark;
+	private String status;
+	private String origin;
+
+	void setName(String name) {
+		this.name = name;
+	}
+
+	void setLaunchDate(String launchDate) {
+		this.launchDate = launchDate;
+	}
+
+	void setMark(int mark) {
+		this.mark = mark;
+	}
+
+	void setStatus(String status) {
+		this.status = status;
+	}
+
+	void setOrigin(String origin) {
+		this.origin = origin;
+	}
 }
--- Lesson_2/JaegerTest.java
+++ Lesson_2/JaegerTest.java
@@ -1,15 +1,15 @@
 public class JaegerTest {
 	public static void main(String[] args) {
 		Jaeger jaeger = new Jaeger();
-		jaeger.name = "Striker Eureka";
-		System.out.println("Имя: " + jaeger.name);
-		jaeger.launchDate = "November 2, 2019";
-		System.out.println("Дата запуска: " + jaeger.launchDate);
-		jaeger.mark = 5;
-		System.out.println("Модель - " + jaeger.mark);
-		jaeger.status = "Destroyed";
-		System.out.println("Статус: " + jaeger.status);
-		jaeger.origin = "Australia";
-		System.out.println("Производитель: " + jaeger.origin);
+		jaeger.setName("Striker Eureka");
+		System.out.println("Имя: " + jaeger.setName());
+		jaeger.setLaunchDate("November 2, 2019");
+		System.out.println("Дата запуска: " + jaeger.setLaunchDate());
+		jaeger.setMark(5);
+		System.out.println("Модель - " + jaeger.setMark(5));
+		jaeger.setStatus("Destroyed");
+		System.out.println("Статус: " + jaeger.setStatus());
+		jaeger.setOrigin("Australia");
+		System.out.println("Производитель: " + jaeger.setOrigin());
 	}
 }
