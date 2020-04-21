f0b3bec0c96b14b9e9cc0f688cba71eea38f2f30
DmitryJav1 <krackin97@gmail.com>
2020-04-21 12:29:48 +0300

    Jaeger.java JaegerTest.java part 1

--- Lesson_2/Jaeger.java
+++ Lesson_2/Jaeger.java
@@ -7,21 +7,26 @@ public class Jaeger {
 
 	void setName(String name) {
 		this.name = name;
+		System.out.println("Name:" + name);
 	}
 
 	void setLaunchDate(String launchDate) {
 		this.launchDate = launchDate;
+		System.out.println("Launch Date:" + launchDate);
 	}
 
 	void setMark(int mark) {
 		this.mark = mark;
+		System.out.println("Mark - " + mark);
 	}
 
 	void setStatus(String status) {
 		this.status = status;
+		System.out.println("Status: " + status);
 	}
 
 	void setOrigin(String origin) {
 		this.origin = origin;
+		System.out.println("Origin: " + origin);
 	}
 }
