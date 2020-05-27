5274350402120a1be992f2cbc42256bfc81a281d
DmitryJav1 <krackin97@gmail.com>
2020-05-27 20:49:07 +0300

    LoopBreaker: added loop while

--- Test_Code_Parts/LoopBreaker.java
+++ Test_Code_Parts/LoopBreaker.java
@@ -4,20 +4,22 @@ public class LoopBreaker {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		String answer;
-		int loopSetup = 3;
+		int attemptCount = 3;
 
-		for(int i = 1;i < loopSetup; i++) {
+		for(int i = 1;i < attemptCount; i++) {
 			System.out.println("I`m LoopBreaker!");
-			System.out.println("Can I help you?");
-			answer = scan.nextLine();
-			if("yes".equals(answer)) {
-				loopSetup+=3;
-			} else if ("no".equals(answer)) {
-				i = loopSetup + i * 2;
-			} else if (answer != "yes" || answer != "no") {
+			while(i < attemptCount) {
 				System.out.println("Can I help you?");
-				answer = scan.nextLine();
-				loopSetup++;
+				answer = scan.nextLine();	
+				if("yes".equals(answer)) {
+					attemptCount++;
+					break;
+				} else if ("no".equals(answer)) {
+					i = attemptCount + 1;
+					break;
+				} else if (answer != "yes" && answer != "no") {
+					i++;
+				}
 			}
 		}
 	}
